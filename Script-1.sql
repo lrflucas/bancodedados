16f5ee691aa9b704c18fbb80ab38f3295c90c3d9
Lucas Rocha <164396709+lrflucas@users.noreply.github.com>
2024-11-04 15:00:07 -0300

    aula 1 de banco de dados

---
+++ Script-1.sql
@@ -0,0 +1,9 @@
+create table minha_tabela (
+	id serial primary key,
+	nome varchar(100),
+	idade int
+);
+
+drop table minha_tabela;
+
+select * from minha_tabela
---
+++ Script.sql
@@ -0,0 +1,1 @@
+create database tropa_cc
