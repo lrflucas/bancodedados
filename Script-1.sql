ff5977f9117fb80e742c5c78948ae14c002ac13f
Lucas Rocha <164396709+lrflucas@users.noreply.github.com>
2024-11-04 16:21:26 -0300

    aula 2 de banco de dados na pratica

--- Script-1.sql
+++ Script-1.sql
@@ -1,9 +1,21 @@
-create table minha_tabela (
+create table pessoa (
 	id serial primary key,
 	nome varchar(100),
 	idade int
 );
 
-drop table minha_tabela;
+select * from pessoa
 
-select * from minha_tabela
+create table enderecos (
+	id serial primary key,
+	id_pessoa int references pessoa(id) on delete cascade,
+	rua varchar(100),
+	cidade varchar(50),
+	estado varchar(50),
+	cep varchar(10)
+);
+
+insert into pessoa (nome, idade) values ('João Silva', 30);
+insert into pessoa (nome, idade) values ('Maria Oliveira', 20);
+insert into pessoa (nome, idade) values ('Carlos Souza', 40);
+insert into pessoa (nome, idade) values ('Ana Costa', 35);
